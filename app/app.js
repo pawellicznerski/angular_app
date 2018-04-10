a89e718d7c39f13a6e635febb2eeb29331de4277
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-04-10 16:33:05 +0200

    routing

---
+++ app/app.js
@@ -0,0 +1,24 @@
+var myapp = angular.module('myapp',['ngRoute']);
+
+myapp.config(['$routeProvider',function($routeProvider){
+
+  $routeProvider.when('/home',{
+      templateurl:'views/home.html',
+      controller:'myappController'
+    }).when('/page1',{
+      templateurl:'views/page1.html'
+    }).when('/page2',{
+      templateurl:'views/page2.html'
+    }).when('/page3',{
+      templateurl:'views/page3.html'
+    }).when('/page4',{
+      templateurl:'views/page4.html'
+    }).otherwise({
+      redirectTo:'/home'
+    });
+
+}]);
+
+myapp.controller('myappController',['$scope',function($scope){
+
+}])
