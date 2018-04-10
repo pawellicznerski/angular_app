0b5a3ca39b7f3d6d9c58cc128d839a2ad49cded7
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-04-10 17:30:33 +0200

    ee

--- app/app.js
+++ app/app.js
@@ -2,16 +2,16 @@ var myapp = angular.module('myapp',['ngRoute']);
 
 myapp.config(['$routeProvider',function($routeProvider){
 
-  $routeProvider.when('/home',{
+  $routeProvider.when('/!',{
       templateurl:'views/home.html',
       controller:'myappController'
-    }).when('/page1',{
+    }).when('#!page1',{
       templateurl:'views/page1.html'
-    }).when('/page2',{
+    }).when('#!page2',{
       templateurl:'views/page2.html'
-    }).when('/page3',{
+    }).when('#!page3',{
       templateurl:'views/page3.html'
-    }).when('/page4',{
+    }).when('#!page4',{
       templateurl:'views/page4.html'
     }).otherwise({
       redirectTo:'/home'
