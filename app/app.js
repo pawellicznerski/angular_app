2e2744dd6b45ca837bfa78603cc8ce84a5236bab
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-04-12 18:41:03 +0200

    adding ui bootstrap, bootstrap animation and initiating styling navbar

--- app/app.js
+++ app/app.js
@@ -1,25 +1,45 @@
-var myapp = angular.module('myapp',['ngRoute']);
+var myapp = angular.module('myapp',['ngRoute','ngAnimate','ui.bootstrap']);
 
 
 myapp.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "views/page1.html"
+        templateUrl : "views/home.html",
+        controller:'myappController',
     })
     .when("/page1", {
-        templateUrl : "views/page2.html"
+        templateUrl : "views/page1.html"
     })
     .when("/page2", {
-        templateUrl : "views/page3.html"
+        templateUrl : "views/page2.html",
+        controller:'myappController'
     })
     .when("/page3", {
-        templateUrl : "views/page4.html"
+        templateUrl : "views/page3.html"
+
     });
 });
 
+angular.module('myModule', ['ui.bootstrap']);
+myapp.controller('CollapseDemoCtrl', function ($scope) {
+  $scope.isNavCollapsed = true;
+  $scope.isCollapsed = false;
+  $scope.isCollapsedHorizontal = false;
+});
+myapp.directive('mainManu',function(){
+  return {
+    restrict:'E',
+    scope:{
+      data:'=',
+      title:'='
+    },
+    templateUrl:"mainMenu.html"
+    // controller: function($scope){}
+  }
+})
 
 myapp.controller('myappController',['$scope',function($scope){
-  $scope.data = {
+  $scope.data = [
     {
       project: "ping-pong",
       url:'ksowkpos'
@@ -28,7 +48,7 @@ myapp.controller('myappController',['$scope',function($scope){
       project: "ping-pong",
       url:'ksowkpos'
     }
-  }
+  ]
 
   $scope.animateBlocks= function(){
     console.log('animation will start');
---
+++ directives/mainMenu.js
@@ -0,0 +1,11 @@
+myapp.directive('mainManu',function(){
+  return {
+    restrict:'E',
+    scope:{
+      data:'=',
+      title:'='
+    },
+    templateUrl:"mainMenu.html"
+    // controller: function($scope){}
+  }
+})
